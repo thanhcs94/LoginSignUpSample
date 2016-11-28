ce91dabc9d9a873684093f56d26e0d278b13c369
thanhcs94 <nguyenvanthanh9294@gmail.com>
2016-11-28 19:14:15 +0700

    back to this project  : 28/11/2016

--- app/src/main/java/com/thanhcs94/loginsignupsample/LoginActivity.java
+++ app/src/main/java/com/thanhcs94/loginsignupsample/LoginActivity.java
@@ -3,23 +3,25 @@ package com.thanhcs94.loginsignupsample;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
+import android.content.Intent;
+import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.net.wifi.WifiInfo;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.app.LoaderManager.LoaderCallbacks;
-
 import android.content.CursorLoader;
 import android.content.Loader;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.AsyncTask;
-
 import android.os.Build;
 import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.text.TextUtils;
+import android.util.Base64;
 import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
@@ -29,13 +31,33 @@ import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
-import com.facebook.network.connectionclass.ConnectionClassManager;
-import com.facebook.network.connectionclass.ConnectionQuality;
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.Profile;
+import com.facebook.ProfileTracker;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+
+import butterknife.Bind;
 
 import static android.Manifest.permission.READ_CONTACTS;
 
@@ -66,9 +88,25 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
     private EditText mPasswordView;
     private View mProgressView;
     private View mLoginFormView;
+    ImageView imgFbLogin;
+    @Bind(R.id.login_facebook)
+    LoginButton fbLoginFB;
+    @Bind(R.id.fb_login_button)
 
+
+    private static final String TAG = LoginActivity.class.getSimpleName();
+
+    String socialEmail;
+    String socialBirthday;
+    String socialGender;
+
+    //facebook
+    private CallbackManager callbackManager;
+    LoginButton loginButton;
+    String accessToken , uId , type;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        FacebookSdk.sdkInitialize(getApplicationContext());
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         // Set up the login form.
@@ -97,10 +135,123 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
 
         mLoginFormView = findViewById(R.id.login_form);
         mProgressView = findViewById(R.id.login_progress);
-        
+        imgFbLogin.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                fbLoginFB.performClick();
+            }
+        });
+
+        /***
+         * this place for Facebook Login
+         * Start Fb_login
+         */
+        getHashCode();
+        //facebook
+        loginButton.setReadPermissions(Arrays.asList("public_profile", "email", "user_birthday", "user_friends"));
+        callbackManager = CallbackManager.Factory.create();
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(final LoginResult loginResult) {
+                ProfileTracker profileTracker = new ProfileTracker() {
+                    @Override
+                    protected void onCurrentProfileChanged(
+                            Profile oldProfile, final Profile currentProfile) {
+                        try {
+                            type = "Facebook";
+                            uId = currentProfile.getId();
+                            String uName = currentProfile.getName();
+                            final String firstName = currentProfile.getFirstName();
+                            final String lastName = currentProfile.getLastName();
+                            final String uAvata = currentProfile.getProfilePictureUri(400, 400).toString();
+                            String assettoken = "";
+                            AccessToken token = AccessToken.getCurrentAccessToken();
+                            if (token != null) {
+                                assettoken = token.getToken();
+                            }
+                            // App code
+                            accessToken = assettoken;
+                            GraphRequest request = GraphRequest.newMeRequest(
+                                    loginResult.getAccessToken(),
+                                    new GraphRequest.GraphJSONObjectCallback() {
+                                        @Override
+                                        public void onCompleted(JSONObject object, GraphResponse response) {
+                                            Log.v("LoginActivity", response.toString());
+                                            // Application code
+                                            try {
+                                                socialEmail = object.getString("email");
+                                                socialGender = object.getString("gender");
+                                                String sms = "User Name: " + firstName + " " + lastName + " " + socialEmail + " " + socialBirthday + " " + socialGender + " -  " + uId + " - " + uAvata + " - " + accessToken;
+                                                Log.wtf("FACEBOOK", sms);
+                                                if (socialEmail.contains("@")) {
+                                                //    loginwithSocial(accessToken, type, uId, TRACK_SOCIAL_FB);
+                                                }
+                                            } catch (JSONException e) {
+                                                String sms = "User Name: " + firstName + " " + lastName + " " + socialEmail + " " + socialBirthday + " " + socialGender + " -  " + uId + " - " + uAvata + " - " + accessToken;
+                                                //loginwithSocial(accessToken, type, uId, TRACK_SOCIAL_FB);
+                                                Log.wtf("FACEBOOK Error", sms);
+                                                e.printStackTrace();
+                                            }
+                                        }
+                                    });
+                            Bundle parameters = new Bundle();
+                            parameters.putString("fields", "id,name,email,gender,birthday");
+                            request.setParameters(parameters);
+                            request.executeAsync();
+
+//                            Intent intent = new Intent(getApplicationContext(), SignupActivity.class);
+//                            intent.putExtra("name", firstName + " "+ lastName);
+//                            intent.putExtra("token", assettoken);
+//                            intent.putExtra("uid", uId);
+//                            startActivityForResult(intent, REQUEST_SIGNUP);
+                            // loginBySocialAcc();
+                            // SplashScreen.showToast(LoginActivity.this, sms);
+                        } catch (Exception e) {
+                        }
+
+                    }
+
+                };
+
+            }
+
+            @Override
+            public void onCancel() {
+
+            }
+
+            @Override
+            public void onError(FacebookException e) {
+
+            }
+        });
+
+        /***
+         * G++ login
+         * Start G++_login
+         */
 
     }
 
+    /** Facebook **/
+    public void getHashCode() {
+
+        try {
+            PackageInfo info = getPackageManager().getPackageInfo(
+                    "your_package",
+                    PackageManager.GET_SIGNATURES);
+            for (Signature signature : info.signatures) {
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                Log.wtf("KeyHash:", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+            }
+        } catch (PackageManager.NameNotFoundException e) {
+
+        } catch (NoSuchAlgorithmException e) {
+
+        }
+
+    }
     private void populateAutoComplete() {
         if (!mayRequestContacts()) {
             return;
@@ -131,6 +282,24 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
         return false;
     }
 
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        Log.e(TAG, "onActivityResult:" + requestCode + ":" + resultCode + ":" + data);
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+//        if (requestCode == RC_SIGN_IN) {
+//            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+//            handleSignInResult(result);
+//        }
+//
+//        if (requestCode == REQ_SIGN_IN_REQUIRED && resultCode == RESULT_OK) {
+//            // We had to sign in - now we can finish off the token request.
+//            new RetrieveTokenTask().execute(gMailPlus);
+//        }
+
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
     /**
      * Callback received when a permissions request has been completed.
      */
@@ -354,4 +523,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
         }
     }
 }
-
+/***
+ *
+ * for FB Login : https://code.tutsplus.com/tutorials/quick-tip-add-facebook-login-to-your-android-app--cms-23837
+ */
